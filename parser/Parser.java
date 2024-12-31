a3f6d0feff9c9e1733711e634799a3e8f6db275a
Talfaza <charifyahia4@gmail.com>
2024-12-31 09:49:21 +0100

    add: double default type 0 return value := instead of =

--- parser/Parser.java
+++ parser/Parser.java
@@ -4,10 +4,10 @@ public class Parser {
     // Had class kaydir parsing dyal tokens, katban w kat-handle expressions w variables
     private TokenManager tm; // TokenManager li kay manage tokens
     private String tc; // Current token
-    private Map<String, Integer> variables; // HashMap li kaykhzn variables w values dyalhom
+    private Map<String, Double> variables; // HashMap li kaykhzn variables w values dyalhom
 
     // Constructor: tm hwa TokenManager, variables hya map li katkhzn l variables
-    public Parser(TokenManager tm, Map<String, Integer> variables) {
+    public Parser(TokenManager tm, Map<String, Double> variables) {
         this.tm = tm;
         this.variables = variables;
         avancer(); // Kaydi current token
@@ -51,19 +51,19 @@ public class Parser {
         if (tc.equals("print")) { // Handle print command
             consommer("print");
             consommer("(");
-            int result = Expression(); // Hitach print katkhsm expression
+            double result = Expression(); // Hitach print katkhsm expression
             consommer(")");
             System.out.println("Result: " + result); // T-print result
         } else if (isVariable(tc)) { // Check ila l token variable
             String varName = tc; // Smya dyal variable
             System.out.println("variable: " + varName);
             consommer(tc); // T-consomme l variable
-            if (tc.equals("=")) { // Check "="
-                consommer("="); // Tconsommer '='
-                int value = Calculat(); // Tcalculi value dyal expression
+            if (tc.equals(":=")) { // Check ":="
+                consommer(":="); // Tconsommer ':='
+                double value = Calculat(); // Tcalculi value dyal expression
                 variables.put(varName, value); // Tstori variable f map
             } else {
-                throw new RuntimeException("Erreur: attendu '=', mais trouvé '" + tc + "'");
+                throw new RuntimeException("Erreur: attendu ':=', mais trouvé '" + tc + "'");
             }
         } else {
             Calculat(); // Ila ma kanch variable kay calculate expression
@@ -74,23 +74,23 @@ public class Parser {
     public void Print() {
         consommer("print");
         consommer("(");
-        int result = Calculat(); // Tcalculi expression li dakhl print
+        double result = Calculat(); // Tcalculi expression li dakhl print
         consommer(")");
         System.out.println("Result: " + result);
     }
 
     // Calculation dyal expression kaml
-    private int Calculat() {
+    private double Calculat() {
         return Expression();
     }
 
     // Hna katdir handle + w -
-    private int Expression() {
-        int result = Term();
+    private double Expression() {
+        double result = Term();
         while (tc.equals("+") || tc.equals("-")) {
             String op = tc;
             consommer(op);
-            int value = Term();
+            double value = Term();
             if (op.equals("+")) {
                 result += value;
             } else if (op.equals("-")) {
@@ -101,12 +101,12 @@ public class Parser {
     }
 
     // Hna katdir handle * w /
-    private int Term() {
-        int result = Factor();
+    private double Term() {
+        double result = Factor();
         while (tc.equals("*") || tc.equals("/")) {
             String op = tc;
             consommer(op);
-            int value = Factor();
+            double value = Factor();
             if (op.equals("*")) {
                 result *= value;
             } else if (op.equals("/")) {
@@ -117,27 +117,27 @@ public class Parser {
     }
 
     // Handle dyal "^" (power)
-    private int Factor() {
-        int result = Base();
+    private double Factor() {
+        double result = Base();
         if (tc.equals("^")) {
             consommer("^");
-            result = (int) Math.pow(result, Factor());
+            result = Math.pow(result, Factor());
         }
         return result;
     }
 
     // Hna kay handle numbers, parentheses, variables, ou print expressions
-    private int Base() {
+    private double Base() {
         if (tc.equals("print")) {
             consommer("print");
             consommer("(");
-            int result = Expression();
+            double result = Expression();
             consommer(")");
             System.out.println("Result: " + result);
             return result;
         } else if (tc.equals("(")) {
             consommer("(");
-            int result = Expression();
+            double result = Expression();
             consommer(")");
             return result;
         } else if (isNumber(tc)) {
@@ -146,7 +146,7 @@ public class Parser {
             String varName = tc;
             consommer(tc);
             if (!variables.containsKey(varName)) {
-                throw new RuntimeException("Erreur: variable '" + varName + "' non définie");
+                return 0.0; // Valeur par défaut hiya 0  
             }
             return variables.get(varName);
         } else {
@@ -155,11 +155,11 @@ public class Parser {
     }
 
     // Handle numbers
-    private int Number() {
+    private double Number() {
         if (isNumber(tc)) {
             String num = tc;
             consommer(tc);
-            return Integer.parseInt(num);
+            return Double.parseDouble(num);
         } else {
             throw new RuntimeException("Erreur: attendu un nombre, mais trouvé '" + tc + "'");
         }
@@ -167,7 +167,7 @@ public class Parser {
 
     // Helper: Check ila token number
     private boolean isNumber(String token) {
-        return token.matches("[0-9]+");
+        return token.matches("[0-9]+(\\.[0-9]+)?");
     }
 
     // Helper: Check ila token variable
