56d20d0cac0e775508a603803f02a51fd04bd9c6
Talfaza <charifyahia4@gmail.com>
2025-01-19 11:07:45 +0100

    add: reading .zt

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -4,13 +4,23 @@
 #include "tokenizer.h"
 #include "parser.h"
 
+bool hasZTExtension(const std::string& filename) {
+    return filename.size() >= 3 && filename.substr(filename.size() - 3) == ".zt";
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <filename>\n";
         return 1;
     }
 
-    std::ifstream file(argv[1]);
+    std::string filename = argv[1];
+    if (!hasZTExtension(filename)) {
+        std::cerr << "Error: The file must have a .zt extension.\n";
+        return 1;
+    }
+
+    std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Error opening file\n";
         return 1;
